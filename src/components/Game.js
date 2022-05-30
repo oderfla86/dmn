a997a6243eb6a389b9670e7e0dafebe589155c75
Alfredo Haddad <alfredohaddad@gmail.com>
2022-05-30 14:23:12 +0100

    Adding logic to play full game up to a 100 points

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect, useReducer } from "react";
+import { useRef, useState, useEffect } from "react";
 import Board from "./Board";
 import Player from "./Player";
 import Score from "./Score";
@@ -57,15 +57,19 @@ function Game(props) {
   };
 
   const DELAY = 1000;
+  const ROUND_DELAY = 5000;
   let leftLeaf = useRef(-1);
   let rightLeaf = useRef(-1);
   let gameBlocked = useRef(0);
   let gameOver = useRef(false);
   let selectedTile = useRef(null);
-  let totalRounds = useRef(0);
-  let startingPlayer = useRef(0);
+  let totalRounds = useRef(props.data.rounds ? props.data.rounds : 0);
+  let team1 = useRef(props.data.team1Points ? props.data.team1Points : 0);
+  let team2 = useRef(props.data.team2Points ? props.data.team2Points : 0);
 
-  const [table, setTable] = useState([]);
+  let startingPlayer = useRef(props.data.startingPlayer);
+
+  const [table, setTable] = useState([...[]]);
   const [player1, setPlayer1] = useState(props.data.player1);
   const [player2, setP2] = useState(props.data.player2);
   const [player3, setP3] = useState(props.data.player3);
@@ -75,24 +79,52 @@ function Game(props) {
   const [isPlayer3Blocked, setPlayer3Blocked] = useState(false);
   const [isPlayer4Blocked, setPlayer4Blocked] = useState(false);
   const [isGameOver, setIsGameOver] = useState(false);
-  const [team1Points, setTeam1Points] = useState(0);
-  const [team2Points, setTeam2Points] = useState(0);
+  const [team1Points, setTeam1Points] = useState(team1.current);
+  const [team2Points, setTeam2Points] = useState(team2.current);
 
   useEffect(() => {
-    console.log("Getting starting player");
-    startingPlayer.current = getStsartingPlayer(
-      player1,
-      player2,
-      player3,
-      player4
-    );
-    if (startingPlayer.current !== 0) {
-      console.log("Player starting:", startingPlayer.current);
-      simulateOtherPlayersTurn(startingPlayer.current);
+    console.log("Getting starting player:", startingPlayer.current);
+    if (startingPlayer.current < 0) {
+      startingPlayer.current = getStsartingPlayer(
+        player1,
+        player2,
+        player3,
+        player4
+      );
+      if (startingPlayer.current !== 0) {
+        console.log("Player starting:", startingPlayer.current);
+        simulateOtherPlayersTurn(startingPlayer.current);
+      }
+    } else {
+      if (startingPlayer.current !== 0) {
+        simulateOtherPlayersTurn(startingPlayer.current);
+      }
     }
   }, []);
-  function timeout() {
-    return new Promise((res) => setTimeout(res, DELAY));
+
+  function timeout(time) {
+    return new Promise((res) => setTimeout(res, time));
+  }
+
+  async function prepareNextRound() {
+    await timeout(ROUND_DELAY);
+    debugger;
+    if (team1.current >= 100 || team2.current >= 100) {
+      console.info("GAME IS FINISHED");
+      console.info("TEAM 1:", team1.current);
+      console.info("TEAM 2:", team2.current);
+    } else {
+      gameOver.current = false;
+      setIsGameOver(false);
+      console.log("team1Points:", team1.current);
+      console.log("team2Points:", team2.current);
+      props.createNewHands(
+        totalRounds.current,
+        team1.current,
+        team2.current,
+        startingPlayer.current
+      );
+    }
   }
 
   function getTileHandIndex(tile) {
@@ -103,7 +135,7 @@ function Game(props) {
     }
   }
 
-  function boardTilePressed(tile) {
+  async function boardTilePressed(tile) {
     //leftValue -1 left side clicked
     //leftValue -2 right side clicked
     gameBlocked.current = 0;
@@ -127,9 +159,12 @@ function Game(props) {
     setTable([...updatedPlayStatus.table]);
     setPlayer1([...disablePlayerHand(player1)]);
     if (player1.length === 0) {
+      console.log("Round is over");
+      let points = calculatePointsForWinners(player2, player4);
       setIsGameOver(true);
-      console.log("GAME IS OVER");
-      setTeam1Points(calculatePointsForWinners(player2, player4));
+      team1.current = team1Points + points;
+      setTeam1Points(team1Points + points);
+      prepareNextRound();
     } else {
       simulateOtherPlayersTurn(1);
     }
@@ -177,7 +212,7 @@ function Game(props) {
 
   async function simulateOtherPlayersTurn(turn) {
     while (turn < 4) {
-      await timeout();
+      await timeout(DELAY);
       let simulationTurn = searchTileForSimulation(
         getPlayerHand(turn),
         leftLeaf.current,
@@ -199,14 +234,22 @@ function Game(props) {
         setTable([...simulationTurn.table]);
         updateHandData(turn, simulationTurn.hand);
         if (simulationTurn.hand.length === 0) {
-          console.log("Game is Over");
+          console.log("Rounds is over");
           gameOver.current = true;
+          let points = 0;
           if (turn === 1 || turn === 3) {
             //AI won
-            setTeam2Points(calculatePointsForWinners(player1, player3));
+            points = calculatePointsForWinners(player1, player3);
+            team2.current = team2Points + points;
+            setTeam2Points(team2Points + points);
+            // console.log("team2Points:", team2Points + points);
           } else {
-            setTeam1Points(calculatePointsForWinners(player2, player4));
+            points = calculatePointsForWinners(player2, player4);
+            team1.current = team1Points + points;
+            setTeam1Points(team1Points + points);
+            // console.log("team1Points:", team1Points + points);
           }
+
           turn = 5;
         }
         turn += 1;
@@ -215,17 +258,17 @@ function Game(props) {
         switch (turn) {
           case 1:
             setPlayer2Blocked(true);
-            await timeout();
+            await timeout(DELAY);
             setPlayer2Blocked(false);
             break;
           case 2:
             setPlayer3Blocked(true);
-            await timeout();
+            await timeout(DELAY);
             setPlayer3Blocked(false);
             break;
           case 3:
             setPlayer4Blocked(true);
-            await timeout();
+            await timeout(DELAY);
             setPlayer4Blocked(false);
             break;
           default:
@@ -242,10 +285,15 @@ function Game(props) {
             player4
           );
           if (result.winner === 0) {
+            team1.current = team1Points + result.points;
             setTeam1Points(team1Points + result.points);
+            // console.log("team1Points:", team1Points + result.points);
           } else if (result.winner === 1) {
+            team2.current = team2Points + result.points;
             setTeam2Points(team2Points + result.points);
+            // console.log("team2Points:", team2Points + result.points);
           }
+
           gameOver.current = true;
           turn = 5;
         }
@@ -260,13 +308,34 @@ function Game(props) {
       setPlayer1([...playerTurnCheck.playerHand]);
       if (playerTurnCheck.blocked) {
         setPlayer1Blocked(true);
-        await timeout();
+        await timeout(DELAY);
         setPlayer1Blocked(false);
         gameBlocked.current += 1;
-        simulateOtherPlayersTurn(1);
+        if (gameBlocked.current >= 4) {
+          gameOver.current = true;
+          console.error("Game is blocked. No more available moves");
+          let result = calculateBlockedGameWinner(
+            player1,
+            player2,
+            player3,
+            player4
+          );
+          if (result.winner === 0) {
+            team1.current = team1Points + result.points;
+            setTeam1Points(team1Points + result.points);
+          } else if (result.winner === 1) {
+            team2.current = team2Points + result.points;
+            setTeam2Points(team2Points + result.points);
+          }
+          setIsGameOver(gameOver.current);
+          prepareNextRound();
+        } else {
+          simulateOtherPlayersTurn(1);
+        }
       }
     } else {
       setIsGameOver(gameOver.current);
+      prepareNextRound();
     }
   }
 
